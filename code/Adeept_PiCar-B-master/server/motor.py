dc889c48402da6422b9b1c58200d6033c1e6b9e3
bswe <bbollenbacher@mindspring.com>
2019-02-18 20:57:57 -0800

    cleaned up sloppy code for stopping the motors.

--- code/Adeept_PiCar-B-master/server/motor.py
+++ code/Adeept_PiCar-B-master/server/motor.py
@@ -19,17 +19,20 @@ MOTOR_A_PIN2 = 10
 MOTOR_B_PIN1 = 13
 MOTOR_B_PIN2 = 12
 
-FORWARD = 0
+FORWARD  = 0
 BACKWARD = 1
-STOP = 0
 
 pwm_A = 0
 pwm_B = 0
 
 setupCalled = False
 
+
 def setup():     #Motor initialization
     global pwm_A, pwm_B, setupCalled
+    if setupCalled:
+        # should only have to do this once
+        return
     GPIO.setwarnings(True)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(MOTOR_A_EN, GPIO.OUT)
@@ -38,13 +41,11 @@ def setup():     #Motor initialization
     GPIO.setup(MOTOR_A_PIN2, GPIO.OUT)
     GPIO.setup(MOTOR_B_PIN1, GPIO.OUT)
     GPIO.setup(MOTOR_B_PIN2, GPIO.OUT)
-    #if setupCalled:
-    #    return
     try:
         print("creating motor A PWM, pwm_A=%s" %str(pwm_A))
-        pwm_A = GPIO.PWM(MOTOR_A_EN, 1000)
+        pwm_A = GPIO.PWM(MOTOR_A_EN, 100)
         print("creating motor B PWM, pwm_B=%s" %str(pwm_B))
-        pwm_B = GPIO.PWM(MOTOR_B_EN, 1000)
+        pwm_B = GPIO.PWM(MOTOR_B_EN, 100)
     except Exception as e:
         print(e)
         #pass
@@ -52,47 +53,36 @@ def setup():     #Motor initialization
     print("pwm_B=%s" %str(pwm_B))
     setupCalled = True
 
-def motorStop():#Motor stops
-    pass
-    GPIO.output(MOTOR_A_PIN1, GPIO.LOW)
-    GPIO.output(MOTOR_A_PIN2, GPIO.LOW)
-    GPIO.output(MOTOR_B_PIN1, GPIO.LOW)
-    GPIO.output(MOTOR_B_PIN2, GPIO.LOW)
-    GPIO.output(MOTOR_A_EN, GPIO.LOW)
-    GPIO.output(MOTOR_B_EN, GPIO.LOW)
+
+def motorStop():      
+    pwm_A.stop()
+    pwm_B.stop()
 
 
-def motorRight(status, direction, speed):   #Motor 2 positive and negative rotation
-    if status == STOP: 
-        motorStop()
-    else:
-        if direction == FORWARD:
-            GPIO.output(MOTOR_B_PIN1, GPIO.HIGH)
-            GPIO.output(MOTOR_B_PIN2, GPIO.LOW)
-            pwm_B.start(100)
-            pwm_B.ChangeDutyCycle(speed)
-        elif direction == BACKWARD:
-            GPIO.output(MOTOR_B_PIN1, GPIO.LOW)
-            GPIO.output(MOTOR_B_PIN2, GPIO.HIGH)
-            pwm_B.start(0)
-            pwm_B.ChangeDutyCycle(speed)
+def motorRight(direction, speed):   #Motor 2 positive and negative rotation
+    if direction == FORWARD:
+        GPIO.output(MOTOR_B_PIN1, GPIO.HIGH)
+        GPIO.output(MOTOR_B_PIN2, GPIO.LOW)
+        pwm_B.start(100)
+        pwm_B.ChangeDutyCycle(speed)
+    elif direction == BACKWARD:
+        GPIO.output(MOTOR_B_PIN1, GPIO.LOW)
+        GPIO.output(MOTOR_B_PIN2, GPIO.HIGH)
+        pwm_B.start(0)
+        pwm_B.ChangeDutyCycle(speed)
 
 
-def motorLeft(status, direction, speed):   #Motor 1 positive and negative rotation
-    if status == STOP: 
-        motorStop()
-    else:
-        if direction == FORWARD:
-            GPIO.output(MOTOR_A_PIN1, GPIO.HIGH)
-            GPIO.output(MOTOR_A_PIN2, GPIO.LOW)
-            pwm_A.start(100)
-            pwm_A.ChangeDutyCycle(speed)
-        elif direction == BACKWARD:
-            GPIO.output(MOTOR_A_PIN1, GPIO.LOW)
-            GPIO.output(MOTOR_A_PIN2, GPIO.HIGH)
-            pwm_A.start(0)
-            pwm_A.ChangeDutyCycle(speed)
-    return direction
+def motorLeft(direction, speed):   #Motor 1 positive and negative rotation
+    if direction == FORWARD:
+        GPIO.output(MOTOR_A_PIN1, GPIO.HIGH)
+        GPIO.output(MOTOR_A_PIN2, GPIO.LOW)
+        pwm_A.start(100)
+        pwm_A.ChangeDutyCycle(speed)
+    elif direction == BACKWARD:
+        GPIO.output(MOTOR_A_PIN1, GPIO.LOW)
+        GPIO.output(MOTOR_A_PIN2, GPIO.HIGH)
+        pwm_A.start(0)
+        pwm_A.ChangeDutyCycle(speed)
 
 
 def destroy():
